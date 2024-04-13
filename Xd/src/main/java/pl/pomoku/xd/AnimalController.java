1de20ce420b4062d36ae50870068a580f382624a
HubiRto <hubert.rybicki.hr@gmail.com>
2024-04-14 00:00:56 +0200

    Zapomniałem dodać application.yml do ustawienia połączenia z bazą danych

--- Xd/src/main/java/pl/pomoku/xd/AnimalController.java
+++ Xd/src/main/java/pl/pomoku/xd/AnimalController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/v1/animal")
+@RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
 public class AnimalController {
     private final AnimalRepository animalRepository;
